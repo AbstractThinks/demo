cf706508632a699879dd7b2362ef41317d9e3021
Jesse Zhuang <408455428@qq.com>
2016-11-24 14:39:42 +0800

    20161124

---
+++ henry/Admin/semantic/js/person.js
@@ -0,0 +1,24 @@
+$(document).ready( function () {
+    init();
+});
+
+
+function init() {
+    $('.ui.sidemenu').on('click', function () {
+        $('.sidemenu.sidebar')
+          .sidebar('setting', 'transition', 'scale down')
+          .sidebar('toggle');
+    });
+
+
+    $('.my-header .menu a.ui.item')
+        .popup({
+            popup : $('.my-header .menu .popup'),
+            on    : 'click',
+            position: 'bottom right',
+            offset: -10
+        });
+
+    $('.ui.rating').rating();
+    $('.menu.my-tab .item').tab();
+}
