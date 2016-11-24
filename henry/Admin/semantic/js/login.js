0f4b49fb7669deced1e81ff74891a1f969501615
JesseZhuang <408455428@qq.com>
2016-11-24 08:28:28 +0800

    20161124

--- henry/Admin/semantic/js/login.js
+++ henry/Admin/semantic/js/login.js
@@ -4,15 +4,53 @@ $(document).ready(function () {
 
 
 function init() {
-    $('.submit.button').on('click', function () {
-        var account = $('#account').val();
-        var psd = $('#password').val();
-        $.get('http://k12.iyunbei.com/api/login', "username="+account+"&password="+psd, function (data) {
-            if (data.error) {
-
-            } else {
-                location.href = "./index.html"
-            }
-        })
+    $('.ui.form').submit(function(e){
+        return false;
     });
+    $('.ui.form').form({
+        inline:true,
+        on: 'blur',
+        transition: 'fade up',
+        fields: {
+            username: {
+                identifier: 'username',
+                rules: [{
+                    type: 'empty',
+                    prompt: '请输入用户名'
+                }]
+            },
+            password: {
+                identifier: 'password',
+                rules: [{
+                    type: 'empty',
+                    prompt: '请输入密码'
+                }]
+            },
+
+        },
+        onSuccess: function() {
+            var username = $('#username').val();
+            var password = $('#password').val();
+            var url_addr = 'http://k12.iyunbei.com/api/login?username='+username+'&password='+password;
+            $.ajax({
+                url:'http://k12.iyunbei.com/api/login',
+                type: 'post',
+                data:{"username":"admin","password":"123456"},
+                dataType: 'json',
+                cache: false,
+                success: function (data) {
+                    if (data.error) {
+                        console.log('帐户不存在');
+                    } else {
+                        location.href = "./index.html";
+                    }
+                },
+                error: function (e) {
+                    console.log(e)
+                }
+
+            })
+        }
+    });
+
 }
