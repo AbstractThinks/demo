223d07deff94293fa40c984b022da91b58b36280
Jesse Zhuang <408455428@qq.com>
2016-12-05 14:40:26 +0800

    20161205

--- henry/Admin/semantic/js/staff.js
+++ henry/Admin/semantic/js/staff.js
@@ -9,22 +9,22 @@ function init() {
                 .sidebar('toggle');
         });
         $('.ui.dropdown').dropdown();
-        $('.add-item').on('click', function () {
-            $('.ui.modal.info').modal('show');
-        });
-        $('.modify-item').on('click', function () {
-            $('.ui.modal.modify').modal('show');
-        });
-        $('.search-item').on('click', function () {
-            $('.ui.modal.info').modal('show');
-        });
-        $('.delete-item').on('click', function () {
-            $('.ui.modal.delete').modal('show');
-        });
-        $('.export-item').on('click', function () {
-
-        });
-        $('.import-item').on('click', function () {
-            $('.ui.modal.file').modal('show');
-        });
+        // $('.add-item').on('click', function () {
+        //     $('.ui.modal.info').modal('show');
+        // });
+        // $('.modify-item').on('click', function () {
+        //     $('.ui.modal.modify').modal('show');
+        // });
+        // $('.search-item').on('click', function () {
+        //     $('.ui.modal.info').modal('show');
+        // });
+        // $('.delete-item').on('click', function () {
+        //     $('.ui.modal.delete').modal('show');
+        // });
+        // $('.export-item').on('click', function () {
+        //
+        // });
+        // $('.import-item').on('click', function () {
+        //     $('.ui.modal.file').modal('show');
+        // });
 }
